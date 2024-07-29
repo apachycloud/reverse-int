835d2212f511ec9f280e28ef392afc06ffa323b1
apachycloud <sezoran01@gmail.com>
2024-07-29 16:21:51 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let b = n.toString().split('').reverse()
+	b.filter((el) => {
+		if (el == '-') {
+			let a = b.pop(el)
+			a = '+'
+			b.unshift(a)
+		}
+	})
+	let c = b.join('')
+
+	return Number(c)
 }
